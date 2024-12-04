f12f455dba12f8b530b474193b3700b23d3b51fa
ipmaclean <iain.p.m.maclean@gmail.com>
2024-12-04 21:11:43 +0000

    Solved Day 3 Part 1

---
+++ src/main/java/iain/aoc2024/Main.java
@@ -0,0 +1,13 @@
+package iain.aoc2024;
+
+import iain.aoc2024.day01.Day01;
+import iain.aoc2024.day02.Day02;
+import iain.aoc2024.day03.Day03;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Day03.solve();
+    }
+}
---
+++ src/main/java/iain/aoc2024/day03/Day03.java
@@ -0,0 +1,55 @@
+package iain.aoc2024.day03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day03 {
+
+    private static final String INPUT_FILE_NAME = "day03/input.txt";
+
+    private Day03() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static void solve() throws IOException {
+        solvePartOne();
+//        solvePartTwo();
+    }
+
+    private static List<String> getMemory() throws IOException {
+        InputStream inputStream = Day03.class.getClassLoader().getResourceAsStream(INPUT_FILE_NAME);
+        List<String> listOfMemory = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                listOfMemory.add(line);
+            }
+        }
+        return listOfMemory;
+    }
+
+    private static void solvePartOne() throws IOException {
+        long solution = 0;
+        List<String> listOfMemory = getMemory();
+        for (String memory : listOfMemory) {
+            Pattern pattern = Pattern.compile("mul\\(\\d+,\\d+\\)", Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(memory);
+            while (matcher.find()) {
+                String match = matcher.group();
+                match = match.replace("mul(", "");
+                match = match.replace(")", "");
+                long[] values = Arrays.stream(match.split(",")).mapToLong(Long::parseLong).toArray();
+                solution += values[0] * values[1];
+            }
+        }
+        System.out.printf("The solution to part one is %s.%n", solution);
+    }
+}

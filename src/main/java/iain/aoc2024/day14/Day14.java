d7143e27406f85ecbe784c93d63c747bebcadee7
ipmaclean <iain.p.m.maclean@gmail.com>
2024-12-14 10:17:40 +0000

    Solved Day 14 Part 2

--- src/main/java/iain/aoc2024/day14/Day14.java
+++ src/main/java/iain/aoc2024/day14/Day14.java
@@ -10,6 +10,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 @Getter
@@ -45,7 +46,44 @@ public class Day14 {
     }
 
     private void solvePartTwo() {
-        System.out.printf("The solution to part two is %s.%n", 0);
+        int counter = -1;
+        int[] quadrants = new int[]{0, 0, 0, 0};
+        while (!(quadrants[0] - 250 > quadrants[1] &&
+                quadrants[0] - 250 > quadrants[2] &&
+                quadrants[0] - 250 > quadrants[3])) {
+            quadrants = new int[]{0, 0, 0, 0};
+            counter++;
+            for (Robot robot : getRobots()) {
+                Integer quadrant = robot.getQuadrantAfterTime(counter);
+                if (quadrant != null) {
+                    quadrants[quadrant]++;
+                }
+            }
+        }
+        printPretty(counter);
+        System.out.printf("The solution to part two is %s.%n", counter);
+    }
+
+    private void printPretty(int time) {
+        HashSet<Point> positions = new HashSet<>();
+        for (Robot robot : getRobots()) {
+            positions.add(robot.getPositionAfterTime(time));
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(System.lineSeparator());
+        sb.append(time);
+        sb.append(System.lineSeparator());
+        for (int y = 0; y < MAX.y; y++) {
+            for (int x = 0; x < MAX.x; x++) {
+                if (positions.contains(new Point(x, y))) {
+                    sb.append("#");
+                } else {
+                    sb.append(".");
+                }
+            }
+            sb.append(System.lineSeparator());
+        }
+        System.out.println(sb.toString());
     }
 
     private void getInput() throws IOException {

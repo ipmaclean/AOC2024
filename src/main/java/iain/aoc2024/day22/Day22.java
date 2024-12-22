25c5153b9ae3c3939756aabcad7d2da69e178d21
ipmaclean <iain.p.m.maclean@gmail.com>
2024-12-22 09:04:29 +0000

    Solved Day 22 Part 2

--- src/main/java/iain/aoc2024/day22/Day22.java
+++ src/main/java/iain/aoc2024/day22/Day22.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class Day22 {
@@ -34,7 +36,64 @@ public class Day22 {
 
     private void solvePartTwo() {
         long solution = 0;
+        List<HashMap<List<Long>, Long>> sequencesToBananasList = getSequencesToBananasList();
+        HashSet<List<Long>> checkedSequences = new HashSet<>();
+        for (HashMap<List<Long>, Long> sequencesToBananas : sequencesToBananasList) {
+            for (List<Long> sequence : sequencesToBananas.keySet()) {
+                if (checkedSequences.contains(sequence)) {
+                    continue;
+                }
+                long totalBananas = 0;
+                for (HashMap<List<Long>, Long> sequencesToBananasInner : sequencesToBananasList) {
+                    totalBananas += sequencesToBananasInner.getOrDefault(sequence, 0L);
+                }
+                checkedSequences.add(sequence);
+                solution = Long.max(solution, totalBananas);
+            }
+        }
         System.out.printf("The solution to part two is %s.%n", solution);
+    }
+
+    private List<HashMap<List<Long>, Long>> getSequencesToBananasList() {
+        List<HashMap<List<Long>, Long>> sequencesToBananasList = new ArrayList<>();
+        for (long secretNumber : secretNumbers) {
+            HashMap<List<Long>, Long> sequencesToBananas = new HashMap<>();
+            sequencesToBananasList.add(sequencesToBananas);
+            long lastPrice = secretNumber % 10;
+
+            secretNumber = getNextSecretNumber(secretNumber);
+            long currentPrice = secretNumber % 10;
+            long diff1 = currentPrice - lastPrice;
+            lastPrice = currentPrice;
+
+            secretNumber = getNextSecretNumber(secretNumber);
+            currentPrice = secretNumber % 10;
+            long diff2 = currentPrice - lastPrice;
+            lastPrice = currentPrice;
+
+            secretNumber = getNextSecretNumber(secretNumber);
+            currentPrice = secretNumber % 10;
+            long diff3 = currentPrice - lastPrice;
+            lastPrice = currentPrice;
+
+            for (int i = 0; i < 2000 - 3; i++) {
+                secretNumber = getNextSecretNumber(secretNumber);
+                currentPrice = secretNumber % 10;
+                long diff4 = currentPrice - lastPrice;
+
+                List<Long> diffList = List.of(diff1, diff2, diff3, diff4);
+                long finalCurrentPrice = currentPrice;
+                sequencesToBananas.computeIfAbsent(diffList,
+                        key -> finalCurrentPrice
+                );
+
+                lastPrice = currentPrice;
+                diff1 = diff2;
+                diff2 = diff3;
+                diff3 = diff4;
+            }
+        }
+        return sequencesToBananasList;
     }
 
     private long getNextSecretNumber(long secretNumber) {
